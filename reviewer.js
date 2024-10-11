a8c16feba4bb1f53ddb1d50d76036700eca61e55
YunHa <pgup10753@gmail.com>
2024-10-11 11:19:02 +0900

    compareCommits 사용

--- reviewer.js
+++ reviewer.js
@@ -18,133 +18,108 @@ import * as dotenv from 'dotenv';
 
     const openai = new OpenAI({
         apiKey: process.env.OPENAI_API_KEY
-    });;
+    });
 
-    // PR의 최신 커밋 SHA 가져오기
-    async function getCommitId(owner, repo, pull_number) {
-        const { data: commits } = await octokit.pulls.listCommits({
-            owner,
-            repo,
-            pull_number
-        });
-
-        // 최신 커밋의 SHA 반환
-        return commits[commits.length - 1].sha;
-    }
-
-    // PR의 diff 가져오기
-    async function getDiff(owner, repo, pull_number) {
-        const { data: files } = await octokit.pulls.listFiles({
-            owner,
-            repo,
-            pull_number
-        });
-
-        let changes = [];
-        files.forEach(file => {
-            if (file.patch) {
-                const patchLines = file.patch.split('\n');
-                let currentBlock = null;
-
-                patchLines.forEach((line, index) => {
-                    if (line.startsWith('+') && !line.startsWith('+++')) {
-                        // 블록이 없으면 새 블록 시작
-                        if (!currentBlock) {
-                            currentBlock = {
-                                file: file.filename,
-                                start_position: index + 1, // 첫 라인의 위치
-                                lines: []
-                            };
-                        }
-                        currentBlock.lines.push(line);  // 블록에 라인 추가
-                    } else {
-                        // 연속된 + 라인 끝, 블록을 저장하고 초기화
-                        if (currentBlock) {
-                            currentBlock.end_position = index;  // 마지막 라인의 위치
-                            changes.push(currentBlock);
-                            currentBlock = null;
-                        }
+    // 전체 과정
+    async function runReview(owner, repo, pull_number, base, head) {
+        try {
+            const MAX_PATCH_COUNT = process.env.MAX_PATCH_LENGTH
+                ? +process.env.MAX_PATCH_LENGTH
+                : Infinity;
+            // 두 커밋 간의 변경 사항 가져오기 (compareCommits 사용)
+            const { data } = await octokit.repos.compareCommits({
+                owner: owner,
+                repo: repo,
+                base: base,
+                head: head,
+            })
+            let { files: changedFiles, commits } = data;
+            if (commits.length >= 2) {
+                const { data: { files }, } = await octokit.repos.compareCommits({
+                    owner: owner,
+                    repo: repo,
+                    base: commits[commits.length - 2].sha,
+                    head: commits[commits.length - 1].sha
+                })
+                const ignoreList = (process.env.IGNORE || process.env.ignore || '')
+                    .split('\n')
+                    .filter((v) => v !== '');
+                const filesNames = files?.map((file) => file.filename) || [];
+                changedFiles = changedFiles?.filter((file) => filesNames.includes(file.filename) &&
+                    !ignoreList.includes(file.filename));
+            }
+            if (!changedFiles?.length) {
+                console.log('no change found');
+                return 'no change';
+            }
+            // 변경사항이 있으면 각 변경사항마다 codeReview 진행
+            for (let i = 0; i < changedFiles.length; i++) {
+                const file = changedFiles[i];
+                const patch = file.patch || '';
+                if (!patch || patch.length > MAX_PATCH_COUNT) {
+                    console.log(`${file.filename} skipped caused by its diff is too large`);
+                    continue;
+                }
+                try {
+                    const res = await codeReview(patch);
+                    if (!!res) {
+                        await octokit.pulls.createReviewComment({
+                            repo: repo,
+                            owner: owner,
+                            pull_number: pull_number,
+                            commit_id: commits[commits.length - 1].sha,
+                            path: file.filename,
+                            body: res,
+                            position: patch.split('\n').length - 1,
+                        });
                     }
-                });
-
-                // 마지막 블록 처리
-                if (currentBlock) {
-                    currentBlock.end_position = patchLines.length;
-                    changes.push(currentBlock);
+                    console.log("Review comments posted successfully!");
+                }
+                catch (e) {
+                    console.error(`review ${file.filename} failed`, e);
                 }
             }
-        });
-
-        return changes;
+        } catch (error) {
+            console.error(`Error comparing commits: ${base}...${head}`);
+            console.error(error.message);
+            throw error; // 다시 예외를 던져서 상위 함수에서 처리할 수 있게함
+        }
     }
 
-    // OpenAI API를 통해 코드 리뷰 생성
-    async function generateReview(block) {
+    //프롬프트 생성 1단계
+    function generatePrompt(patch) {
         const prompt = `
-        You should answer in Korean.
-        You are a strict and perfect code reviewer. You cannot tell any lies.
-        Please evaluate the code added or changed through Pull Requests.
+        Answer me in Korean.
+        Below is a code patch, please help me do a brief code review on it.
+        Summarize what changes the code patch has.
+        Any but risks and/or improvement suggestions are welcome:
+        `
+        return `${prompt}, ${patch}`;
+    }
 
-        According to the given evaluation criteria, if a code patch corresponds to any of the issues below, give the user a feedback.
-
-        There are four evaluation criteria. If multiple issues correspond to a single criteria, you should address them in a detailed manner:
-            - Feedback should describe what the issue is according to the evaluation criteria.
-            - Relevant_Lines should be written as "[line_num]-[line_num]", indicating the range of lines where the issue occurs.
-            - Suggested_Code should only include the revised code based on the feedback.
-
-        If there are no issues, DO NOT SAY ANYTHING. In that case, your asnwer has to be empty.
-
-        Evaluation criteria are:
-        - Pre-condition_check: Check whether a function or method has the correct state or range of values for the variables needed to operate properly.
-        - Runtime Error Check: Check code for potential runtime errors and identify other possible risks.
-        - Security Issue: Check if the code uses modules with serious security flaws or contains security vulnerabilities.
-        - Optimization: Check for optimization points in the code patch. If the code is deemed to have performance issues, recommend optimized code.
-
-        Your answer should be in Korean.
-
-        Code to review:
-        ${block.lines.join('\n')}
-        `;
-
+    // 프롬프트 만들고 응답 받아오는 2단계
+    async function codeReview(patch) {
+        if (!patch) { return ''; }
+        const prompt = generatePrompt(patch);
+        // prompt가 문자열인지 확인
+        if (typeof prompt !== 'string') {
+            throw new Error("Generated prompt is not a string");
+        }
+        // 응답 생성
         const response = await openai.chat.completions.create({
-            model: "gpt-4",
+            model: "gpt-4o",
             messages: [{ role: "system", content: prompt }],
             max_tokens: 1000,
             temperature: 0,
         });
-
         return response.choices[0].message.content;
-    }
-
-    // PR에 리뷰 게시
-    async function postReviewComment(owner, repo, pull_number, commit_id, file, start_position, review_body) {
-        await octokit.pulls.createReviewComment({
-            owner: owner,
-            repo: repo,
-            pull_number: pull_number,
-            body: review_body,
-            path: file,
-            position: start_position,  // 첫 라인의 위치
-            commit_id: commit_id  // 최신 커밋 SHA 추가
-        });
     }
 
 
     // 전체 리뷰 생성 및 게시 프로세스
-    async function reviewPullRequest(owner, repo, pull_number) {
+    async function reviewPullRequest(owner, repo, pull_number, base, head) {
         try {
-            const commit_id = await getCommitId(owner, repo, pull_number); // 최신 커밋 SHA 가져오기
-            const changes = await getDiff(owner, repo, pull_number);
-
-            // 각 블록별로 리뷰 생성 및 코멘트 게시
-            for (const block of changes) {
-                const review = await generateReview(block); // 블록 자체 전달
-                if (review) {
-                    await postReviewComment(owner, repo, pull_number, commit_id, block.file, block.start_position, review);
-                }
-            }
-
-            console.log("Review comments posted successfully!");
+            runReview(owner, repo, pull_number, base, head)
         } catch (error) {
             console.error("Error:", error);
         }
@@ -154,7 +129,9 @@ import * as dotenv from 'dotenv';
     const owner = process.env.GITHUB_OWNER;  // GitHub 사용자 또는 조직 이름
     const repo = process.env.GITHUB_REPOSITORY_NAME;  // 리포지토리 이름
     const pull_number = process.env.GITHUB_PR_NUMBER;  // PR 번호
+    const base = process.env.GITHUB_BASE_COMMIT;  // 비교할 기준 커밋
+    const head = process.env.GITHUB_HEAD_COMMIT;  // 비교할 최신 커밋
 
-    reviewPullRequest(owner, repo, pull_number);
+    reviewPullRequest(owner, repo, pull_number, base, head);
 
 })();
